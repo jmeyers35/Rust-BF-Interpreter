376fe20c55a24fb93d444f5fbec292162a311a5a
Jacob Meyers <jacobmeyers065@gmail.com>
2020-03-06 06:19:19 -0500

    fixed loop bug, fixed byte wraparound, added hanoi.bf

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use clap::{Arg, App};
 use std::fs;
 
-mod defs;
+mod lib;
+use lib::ExecutionContext;
 
 fn main() -> std::io::Result<()> {
     let matches = App::new("Brainfuck Interpeter")
-                    .arg(Arg::with_name("INPUT")
+                    .arg(Arg::with_name("FILE")
                         .help("Brainfuck source file.")
                         .required(true)
                         .index(1))
                 .get_matches();
 
-    let src_fname = matches.value_of("INPUT").unwrap();
+    let src_fname = matches.value_of("FILE").unwrap();
     let src_file_contents = fs::read(src_fname)?;
 
-    let mut ctx = defs::ExecutionContext::new(src_file_contents);
+    let mut ctx = ExecutionContext::new(src_file_contents);
 
     // TODO: add configs for stuff like debugging and single stepping?
     ctx.execute_all();
